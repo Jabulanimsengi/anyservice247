092749e17c2be2d5f8c186cf713c058bd9e5e174
Jabulani Msengi <tsakanimsengi@gmail.com>
2025-08-10 02:19:08 +0200

    Added building to the categories

--- src/lib/categories.ts
+++ src/lib/categories.ts
@@ -1,7 +1,7 @@
 // src/lib/categories.ts
 
 export const categories = [
-  "Plumbing", "Electrical", "Carpentry", "Painting", "Gardening",
+  "Plumbing", "Building", "Electrical", "Carpentry", "Painting", "Gardening",
   "Cleaning", "Appliance Repair", "Roofing", "Pest Control", "Welding", 
   "Gate Repair", "Ceiling Installation", "Door Installation", "Tile Installation", 
   "Kitchen Installation", "TV Stand Installation", "Paving", "Waterproofing", 
